2754911ef53f86c94e3974c25cced30bb71434a1
karenknan <karen.k.nan@gmail.com>
2014-03-17 01:11:29 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$("#schapiro").on("mouseenter", function (e){
+ $("#schapiro").css("fill","yellow");
+  $("#schapiro").css("cursor","pointer");
+
+  });
+$("#schapiro").on("mouseout", function (e){
+ $("#schapiro").css("fill","#007fb2");
+  });
+
+$("#schapiro").on("click", function (e){
+	 $(".housing-info-body").empty();
+	 $(".housing-info-title").empty();
+	 $(".housing-info-title").append("Building details");
+ $(".housing-info-body").append("<h2>Schapiro</h2>");
+ $(".housing-info-body").append("With its upcoming renovations from the sixth floor up, Schapiro’s gotten much more popular for rising juniors. Each floor has a roomy kitchen and lounge, while the building as a whole boasts perhaps the most beautiful sky lounge at Columbia. Expect singles to be grabbed by juniors and doubles by sophomores. (singles, doubles)");
+  });
+
+$("#woodbridge").on("mouseenter", function (e){
+ $("#woodbridge").css("fill","yellow");
+ $("#woodbridge").css("cursor","pointer");
+
+  });
+$("#woodbridge").on("mouseout", function (e){
+ $("#woodbridge").css("fill","#007fb2");
+
+  });
+
+$("#woodbridge").on("click", function (e){
+	 $(".housing-info-body").empty();
+	 	 $(".housing-info-title").empty();
+	 $(".housing-info-title").append("Building details");
+ $(".housing-info-body").append("<h2>Woodbridge</h2>");
+ $(".housing-info-body").append("Seniors and juniors will fill up Woodbridge. Every double in Woodbridge features a private kitchen and bathroom as well as a large living room. (2 person)");
+  });
---
+++ readme.md
@@ -0,0 +1,7 @@
+Shaft Map
+
+SVG Map project for The Shaft at spectrum.columbiaspectator.com
+
+Map created previously in Adobe Illustrator by Spec Graphic Design, exported and cleaned up to web-safe svg
+
+Using modernizr, jquery, svgpan, underscore.js
