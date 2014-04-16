8555f7de2c8525fe3a6acf413165d53b7508eda1
karenknan <karen.k.nan@gmail.com>
2014-04-15 22:53:42 -0400

    live version V1

--- js/main.js
+++ js/main.js
@@ -4,18 +4,28 @@ Shaft.filterCriteria=[];
 Shaft.MatchedDorms=[];
 Shaft.DormObjectMap = {};
 
+
+function is_touch_device() {
+  return 'ontouchstart' in window // works on most browsers 
+      || 'onmsgesturechange' in window; // works on ie10
+};
+
 window.onload = function() {
+if (!is_touch_device()){
   svgPanZoom.init({
     'zoomEnabled': true,
     'minZoom': 0.20,
     'maxZoom': 5
   });
+}
 };
+
 Shaft.DormObject = function (prop) {
     this.name = prop.name;
     this.description= prop.description;
     this.amenities = prop.amenities;
     this.suiteSize = prop.suiteSize;
+    this.cutoffs = prop.cutoffs;
 }
 
 function appendInfo(dorm) {
@@ -27,31 +37,37 @@ function appendInfo(dorm) {
   $(".building-details").css("display","inherit");
   $(".housing-info-body").append("<h2>" + dormObj.name + "</h2>");
   $(".housing-info-body").append(dormObj.description);
-  $(".housing-info-body").append(
+  $(".housing-info-body").append("<div class='cutoff-title'>");
 
+  if (dormObj.cutoffs){
+  $(".cutoff-title").append("2013 Cutoff:");
+}
 
+  //console.log(dormObj.cutoffs);
+  for (var i in dormObj.cutoffs){
+     $(".housing-info-body").append("<div class='cutoff-row'><div class='cutoff-type'>"+dormObj.cutoffs[i].type+"</div><div class='cutoff-cutoff'>"+dormObj.cutoffs[i].cutoff+"</div></div>");
+  }
 
-    )
-  $(".housing-info-body").append("<p>");
+  $(".housing-info-body").append("<div class='icon-result-wrapper'>");
   $(".filter-results").css("display", "none");
   
   if(dormObj.amenities['kitchen']) {
-    $(".housing-info-body").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/double icon-09.png" height="70px" width="69px"> ');
+    $(".icon-result-wrapper").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/double icon-09.png" height="70px" width="69px"> ');
   }
   if(dormObj.amenities['musicRoom']) {
-    $(".housing-info-body").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/Practice Room Icon-01.png" height="70px" width="69px"> ');
+    $(".icon-result-wrapper").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/Practice Room Icon-01.png" height="70px" width="69px"> ');
   }
   if(dormObj.amenities['ac']) {
-    $(".housing-info-body").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/Climate Control Black and White-01.png" height="70px" width="69px"> ');
+    $(".icon-result-wrapper").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/Climate Control Black and White-01.png" height="70px" width="69px"> ');
   }
   if(dormObj.amenities['fitness']) {
-    $(".housing-info-body").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/Fitness Black and White-01.png" height="70px" width="69px"> ');
+    $(".icon-result-wrapper").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/Fitness Black and White-01.png" height="70px" width="69px"> ');
   }
   if(dormObj.amenities['computerLab']) {
-    $(".housing-info-body").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/Computer Black and White-01.png" height="70px" width="69px"> ');
+    $(".icon-result-wrapper").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/Computer Black and White-01.png" height="70px" width="69px"> ');
   }
   if(dormObj.amenities['stallBR']) {
-    $(".housing-info-body").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/bathroom stall icon copy.png" height="70px" width="69px"> ');
+    $(".icon-result-wrapper").append(' <img src="http://spectrum.columbiaspectator.com/shaft_map/img/bathroom stall icon copy.png" height="70px" width="69px"> ');
   }
 
 
@@ -70,11 +86,11 @@ function appendInfo(dorm) {
 
 
 function appendFilterResults(){
-  console.log("filter");
+  //console.log("filter");
   var matchedArray=Shaft.MatchedDorms;
   $(".filter-results").empty();
   for (var i in matchedArray){
-    console.log(matchedArray[i]);
+    //console.log(matchedArray[i]);
     var resultObj=Shaft.DormObjectMap[matchedArray[i]];
     $(".filter-results").append("<div class='result "+matchedArray[i]+"'>"+resultObj.name+"</div>");
     makeClickableResults(matchedArray[i]);
@@ -82,7 +98,7 @@ function appendFilterResults(){
 }
 
 function makeClickableResults(dorm){
- $("."+dorm).on("click", function(){ appendInfo(dorm); });
+ $("."+dorm).on("click", function(){ appendInfo(dorm) });
 }
 
 function makeClickable(id, dorm) {
@@ -127,7 +143,7 @@ function addFilter(filter){
   }
   if (!addedFilter){
   Shaft.filterCriteria.push(filter);
-  console.log(Shaft.filterCriteria);
+  //console.log(Shaft.filterCriteria);
   }
 }
 
@@ -169,7 +185,7 @@ function matchDorm(filter, isActive){
       Shaft.MatchedDorms.push(dorm);
     }
   }
-  console.log(Shaft.MatchedDorms);
+  //console.log(Shaft.MatchedDorms);
   appendFilterResults();
 }
 
@@ -216,6 +232,15 @@ makeClickable('cathedral', 'cathedral');
 var schapiro= {
   'name': "Schapiro",
   'description': "After undergoing renovations the past two summers, Schapiro is in tip-top shape to be a hugely popular pick among seniors and juniors (for the singles) and sophomores (for the doubles). Great views, two big lounges, music practice rooms, and workout equipment are just some of the ways Schapiro residents are spoiled. Sophomores looking at Schapiro’s doubles should beware, though, that the walkthrough doubles labeled on floor plans do not have a dividing door, and the setup can be tricky and claustrophobic for some.",
+  'cutoffs':[{
+    type:"Single",
+    cutoff:"20/2283"
+  },
+  {
+    type:"Double",
+    cutoff:"10/2492"
+  }
+  ],
   'amenities': {
       'kitchen': true,
       'musicRoom': true,
@@ -242,6 +267,16 @@ var schapiro= {
 var woodbridge= {
   'name': "Woodbridge",
   'description': "The only Columbia dorm on Riverside Drive, Woodbridge provides some of the best apartment housing available to students. The two-person apartments can be turned into a kind of walkthrough double with one person in the bedroom and another in the living area. The windy walk down 115th might deter some, but for those who can make the trek, Woodbridge offers that “off-campus” feel many are looking for with junior and senior housing.",
+  'cutoffs':[
+  {
+    type:"1-Bedroom Apartment",
+    cutoff: "20/1446"
+  },
+  {
+    type:"2-Bedroom Apartment",
+    cutoff: "30/1234"
+  }
+  ],
   'amenities': {
       'kitchen': true,
       'musicRoom': false,
@@ -375,6 +410,28 @@ var sulz = {
 var claremont = {
   'name': "47 Claremont",
   'description': "Arguably the most isolated dorm at Columbia, Claremont rewards its residents for braving the wind tunnel of Claremont Avenue by offering suites with full kitchens. Suite bathrooms were renovated last year and have been described as “hotel bathrooms.” Claremont is also a pretty good place to host parties—nothing like EC, but if you can get people to come this far north, it’s a decent setup.",
+  'cutoffs':[{
+    type: "3-person",
+    cutoff: "20/342"
+  },
+  {
+    type: "4-person",
+    cutoff: "30/2224"
+  },
+  {
+    type: "5-person",
+    cutoff: "24/1307"
+  },
+  {
+    type: "6-person",
+    cutoff: "20/849"
+  },
+  {
+    type: "7-person",
+    cutoff:"10/1705"
+  }
+
+  ],
   'amenities': {
       'kitchen': true,
       'musicRoom': false,
@@ -434,7 +491,7 @@ var ec = {
 
     {
       'type': "6th floor double", 
-      'cutoff':"30/1309"
+      'cutoff':"15/401"
     }
 
 
@@ -521,9 +578,9 @@ var n620w116 = {
       'kitchen': true,
       'musicRoom': false,
       'ac': false,
-      'computerLab': true,
+      'computerLab': false,
       'fitness': false,
-      'stallBR': true,
+      'stallBR': false,
       'single': true,
       'double': true
   },
@@ -543,6 +600,14 @@ var n620w116 = {
 var wien = {
   'name': "Wien",
   'description': "Wien gets trash-talked more than it deserves. Decently sized singles, doubles, and walkthrough doubles make up each of these floors, which share a bathroom. Don’t forget, though, that each room has its own sink—a quirk only of Wien rooms that proves pretty useful, whether you love to brush your teeth or don’t like walking all the way to the hall bathroom (if you know what I mean). One thing to keep in mind is that not every floor has a kitchen, which can be a dealbreaker for some juniors.",
+  'cutoffs': [{
+    type:"Single",
+    cutoff:"10/2932"
+  },
+  {
+    type:"Double",
+    cutoff: "10/2818"
+  }],
   'amenities': {
       'kitchen': true,
       'musicRoom': false,
@@ -569,6 +634,11 @@ var wien = {
 var furnald = {
   'name': "Furnald",
   'description': "Only around 40 lucky sophomores will get to pick into Furnald’s roomy singles. Sacrificing the upperclassman living experience for the best facilities on campus is a fair deal for sophomore Furnaldians, who enjoy great views of Broadway and campus as well as the clean and quiet of Furnald’s bathrooms, kitchens, and lounges. Picking with a group can make the experience of living among first-years less disorienting for the veteran sophomore.",
+  'cutoffs':[{
+    type:"Single",
+    cutoff:"10/930"
+  }
+  ],
   'amenities': {
       'kitchen': true,
       'musicRoom': false,
@@ -595,12 +665,16 @@ var furnald = {
 var river = {
   'name': "River",
   'description': "Large singles and small floors make River a popular choice for seniors and juniors looking to have a quiet and comfortable space to come home to. Next year, Jazz House will take over the first floor, and the remaining rooms—all singles—will be available to seniors and juniors with decent numbers. Though perhaps not the most social of dorms, River is well-located, and residents appreciate sharing a kitchen and bathroom with fewer people than their Schapiro or Broadway counterparts do.",
+  'cutoffs':[{
+    type:"Single",
+    cutoff:"20/1962"
+  }],
   'amenities': {
       'kitchen': true,
       'musicRoom': true,
       'ac': false,
       'computerLab': true,
-      'fitness': false,
+      'fitness': true,
       'stallBR': false,
       'single': true,
       'double': false
@@ -621,6 +695,19 @@ var river = {
 var hogan = {
   'name': "Hogan",
   'description': "Hogan is always the first dorm to be completely picked during selection, and there’s no question why. With all singles in its 4- and 5-person suites, Hogan is a popular choice for seniors hoping to live with friends in a social environment but who aren’t so keen on EC. Hogan doesn’t have AC or the same ease in hosting parties, but residents get the benefits of an excellent location and all the facilities in the adjoining Broadway Residence Hall.",
+  'cutoffs':[{
+    type: "4-person",
+    cutoff: "30/1014"
+
+  },
+  {
+    type:"5-person",
+    cutoff:"30/1257"
+  },
+  {
+    type: "6-person",
+    cutoff: "30/1496"
+  }],
   'amenities': {
       'kitchen': true,
       'musicRoom': true,
@@ -647,6 +734,14 @@ var hogan = {
 var broadway = {
   'name': "Broadway",
   'description': "One of Columbia’s newest-constructed dorms, Broadway is praised for its cleanliness, excellent facilities (with AC!), and beautiful views in all directions. Speedy elevators take junior and senior residents to their singles—some large, some not so large—and a few sophomores to their doubles, some of which are fairly big. Broadway also has the perfect location between academic life (close to Hamilton) and social life (bars and restaurants within just a few blocks).",
+  'cutoffs': [{
+    type:"Single",
+    cutoff:"20/2040"
+  },
+  {
+    type:"Double",
+    cutoff:"10/2893"
+  }],
   'amenities': {
       'kitchen': true,
       'musicRoom': true,
@@ -673,12 +768,26 @@ var broadway = {
 var ruggles = {
   'name': "Ruggles",
   'description': "Juniors who want to live with friends in groups will find themselves in Ruggles, mostly in 8-person suites, with some in doubles and some in singles. Of junior dorms, Ruggles is tied with Watt for best place to host social gatherings, but the trade-off is that many rooms face a shaft. Sharing a kitchen and bathroom with just a few close friends can be a treat, but having eight people living in one suite brings its own set of challenges.",
+  'cutoffs':[{
+    type:"4-person",
+    cutoff:"30/1222"   
+  },
+  {
+    type:"6-person",
+    cutoff:"20/761"
+  },{
+    type:"8-person, 2 singles",
+    cutoff:"20/2918"
+  },{
+    type:"8-person, 4 singles",
+    cutoff: "21.25/700"
+  }],
   'amenities': {
       'kitchen': true,
       'musicRoom': false,
       'ac': false,
       'computerLab': false,
-      'fitness': false,
+      'fitness': true,
       'stallBR': false,
       'single': true,
       'double': true
@@ -699,6 +808,15 @@ var ruggles = {
 var nussbaum = {
   'name': "Nussbaum (600 W. 113)",
   'description': "Named for its location right above Nussbaum & Wu (the bakery), Nussbaum is a popular choice for sophomores hoping to avoid McBain. Large singles—some with private bathrooms—are taken by seniors and juniors, but the social scene of each suite tends to be dominated by sophomores. Newly renovated kitchens are a plus. Some suites have single-use bathrooms, while others have stalls shared by the suite. Keep in mind that you can’t pick your suitemates, but you can choose to live near people you know if you go in as a larger group.",
+  'cutoffs':[{
+    type:"Single",
+    cutoff:"20/1158"
+  },
+  {
+    type:"Double",
+    cutoff:"10/2927"
+  }],
+
   'amenities': {
       'kitchen': true,
       'musicRoom': false,
@@ -725,6 +843,14 @@ var nussbaum = {
 var mcbain = {
   'name': "McBain",
   'description': "Infamous for its less-than-ideal bathrooms and dark, stuffy shaft rooms, McBain has an iffy reputation among rising sophomores, who fill the building’s large doubles. Sometimes called “Carman II,” McBain is pegged as the most social sophomore dorm, since floors are large, rooms are big enough for parties, and nearly all residents are in the same class. Renovations this summer will change the look of the 7th and 8th floors of McBain, with single-use bathrooms being installed, as well as some updates to floor lounges throughout the building, falling ceilings be damned.",
+  'cutoffs':[{
+    type:"Single",
+    cutoff:"10/884"
+  },
+  {
+    type:"Double",
+    cutoff:"End of group selection"
+  }],
   'amenities': {
       'kitchen': true,
       'musicRoom': false,
@@ -751,6 +877,24 @@ var mcbain = {
 var watt = {
   'name': "Watt",
   'description': "Made up mostly of studio doubles, Watt is a popular choice for juniors, as well as seniors, who take the 1- and 2-bedroom apartments—as well as the few studio singles—pretty early during selection. Each apartment has its own kitchen and bathroom—cleaned weekly by Facilities—and square footage is decent throughout. Floors don’t have shared lounges, which can make it hard to get to know floormates, but the size and setup of rooms makes them ideal for social gatherings.",
+  'cutoffs':[
+    {
+      type:"Studio Single",
+      cutoff:"30/238"
+    },
+    {
+      type:"Studio Double",
+      cutoff: "Junior Regroup"
+    },
+    {
+    type:"1-bedroom Apartment",
+    cutoff:"20/55"
+
+  },
+  {
+    type:"2-bedroom Apartment",
+    cutoff:"30/309"
+  }],
   'amenities': {
       'kitchen': true,
       'musicRoom': false,
@@ -777,6 +921,10 @@ var watt = {
 var symposium ={
   'name': "Symposium (548 W. 113)",
   'description': "Hidden on 113th Street, right above Symposium (the Greek restaurant), is one of Columbia Housing’s best-kept secrets. Symposium (officially, 548 W. 113th St.) has several huge studio doubles, which get picked mainly by juniors with good numbers. The only brownstone available in Room Selection, Symposium might not be the most talked-about dorm, but it gives Watt and Woodbridge a run for their money in the contest for best apartment-style living.",
+  'cutoffs':[{
+    type:"Double",
+    cutoff:"20/1104"
+  }],
   'amenities': {
       'kitchen': true,
       'musicRoom': false,
@@ -829,6 +977,14 @@ var n601w110 = {
 var harmony = {
   'name': "Harmony",
   'description': "The butt of many a Columbia housing joke, Harmony is seen as the dregs for juniors and sophomores picking into singles. Except for one 6-person suite, the building is made up of singles and doubles ranging from tiny to enormous. Unless you’re a Westside lover, you might find the walk to campus too long, but the off-campus feel is a big plus for students who don’t love the “dormy” vibe.",
+  'cutoffs':[{
+    type:"Single",
+    cutoff:"10/2992"
+  },
+  {
+    type:"Double",
+    cutoff: "10/2484"
+  }],
   'amenities': {
       'kitchen': true,
       'musicRoom': false,
